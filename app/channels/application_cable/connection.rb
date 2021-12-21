cb0e51d7b82160fb91e9850a7d861b9c01e46953
kajal259 <kajal.logicalstreet@gmail.com>
2021-12-21 11:32:05 +0530

    action_cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,22 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_url
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+    def find_verified_user
+      if verfied_user :User.find_by(id: cookies.encrypted[:user_id])
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end 
+    
+    def report_error(e)
+      SomeExternalBugtrackingService.notify(e)
+    end 
   end
 end
